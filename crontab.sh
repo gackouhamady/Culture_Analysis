110a22462ab06ec3fd2d394ddcea8b256d61effd
hamady.gackou <120125192+gackouhamady@users.noreply.github.com>
2024-12-23 02:24:46 +0100

    cron job fix on venv

--- crontab.sh
+++ crontab.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+ # Active l'environnement virtuel 
+source /home/user/Projet_Linux/Culture_Analysis/myvenv/bin/activate
+
 # Exécution des scripts Bash toutes les 3 heures
 festival_concert/part_freq_concert_festival_bash.sh
 football/performance_sportive_bash.sh
 
+
 # Exécution des scripts Python toutes les 4 heures
 festival_concert/summarize_part_freq_concert_festival.py
-
 football/summarize_performance_sportive.py
 
